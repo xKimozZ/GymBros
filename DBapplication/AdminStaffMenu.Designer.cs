06155083863203b825596f7455fbfa3f8106f029
EzzCode <ezz05042003@gmail.com>
2023-12-21 23:43:57 +0200

    fixes

--- DBapplication/AdminStaffMenu.Designer.cs
+++ DBapplication/AdminStaffMenu.Designer.cs
@@ -30,12 +30,8 @@
         {
             this.panelAdAdmin = new System.Windows.Forms.Panel();
             this.buttonAddadmin = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.panelStaffdel = new System.Windows.Forms.Panel();
             this.buttonDel = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.buttonAdmin = new System.Windows.Forms.Button();
             this.buttonProfile = new System.Windows.Forms.Button();
             this.buttonRemove = new System.Windows.Forms.Button();
@@ -46,8 +42,9 @@
             this.textBoxSalary = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.buttonSalary = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxID = new System.Windows.Forms.ComboBox();
+            this.labelID = new System.Windows.Forms.Label();
+            this.panelAdd = new System.Windows.Forms.Panel();
             this.panelAdAdmin.SuspendLayout();
             this.panelStaffdel.SuspendLayout();
             this.panelManager.SuspendLayout();
@@ -56,74 +53,36 @@
             // panelAdAdmin
             // 
             this.panelAdAdmin.Controls.Add(this.buttonAddadmin);
-            this.panelAdAdmin.Controls.Add(this.label5);
-            this.panelAdAdmin.Controls.Add(this.comboBox3);
-            this.panelAdAdmin.Location = new System.Drawing.Point(272, 99);
+            this.panelAdAdmin.Location = new System.Drawing.Point(272, 168);
             this.panelAdAdmin.Name = "panelAdAdmin";
-            this.panelAdAdmin.Size = new System.Drawing.Size(365, 125);
+            this.panelAdAdmin.Size = new System.Drawing.Size(365, 56);
             this.panelAdAdmin.TabIndex = 24;
             // 
             // buttonAddadmin
             // 
-            this.buttonAddadmin.Location = new System.Drawing.Point(132, 69);
+            this.buttonAddadmin.Location = new System.Drawing.Point(137, 20);
             this.buttonAddadmin.Name = "buttonAddadmin";
             this.buttonAddadmin.Size = new System.Drawing.Size(75, 23);
             this.buttonAddadmin.TabIndex = 2;
             this.buttonAddadmin.Text = "Make Admin";
             this.buttonAddadmin.UseVisualStyleBackColor = true;
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(51, 27);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(18, 13);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "ID";
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(120, 24);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(172, 21);
-            this.comboBox3.TabIndex = 0;
-            // 
             // panelStaffdel
             // 
             this.panelStaffdel.Controls.Add(this.buttonDel);
-            this.panelStaffdel.Controls.Add(this.label3);
-            this.panelStaffdel.Controls.Add(this.comboBox1);
-            this.panelStaffdel.Location = new System.Drawing.Point(272, 99);
+            this.panelStaffdel.Location = new System.Drawing.Point(272, 168);
             this.panelStaffdel.Name = "panelStaffdel";
-            this.panelStaffdel.Size = new System.Drawing.Size(365, 119);
+            this.panelStaffdel.Size = new System.Drawing.Size(365, 50);
             this.panelStaffdel.TabIndex = 23;
             // 
             // buttonDel
             // 
-            this.buttonDel.Location = new System.Drawing.Point(132, 69);
+            this.buttonDel.Location = new System.Drawing.Point(137, 15);
             this.buttonDel.Name = "buttonDel";
             this.buttonDel.Size = new System.Drawing.Size(75, 23);
             this.buttonDel.TabIndex = 2;
             this.buttonDel.Text = "Delete";
             this.buttonDel.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 27);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(18, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "ID";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(120, 24);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(172, 21);
-            this.comboBox1.TabIndex = 0;
             // 
             // buttonAdmin
             // 
@@ -167,6 +126,7 @@
             this.buttonAdd.TabIndex = 19;
             this.buttonAdd.Text = "Add";
             this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // label1
             // 
@@ -184,31 +144,29 @@
             this.panelManager.Controls.Add(this.textBoxSalary);
             this.panelManager.Controls.Add(this.button1);
             this.panelManager.Controls.Add(this.buttonSalary);
-            this.panelManager.Controls.Add(this.label4);
-            this.panelManager.Controls.Add(this.comboBox2);
-            this.panelManager.Location = new System.Drawing.Point(272, 99);
+            this.panelManager.Location = new System.Drawing.Point(272, 168);
             this.panelManager.Name = "panelManager";
-            this.panelManager.Size = new System.Drawing.Size(365, 182);
+            this.panelManager.Size = new System.Drawing.Size(365, 113);
             this.panelManager.TabIndex = 25;
             // 
             // comboBoxRoles
             // 
             this.comboBoxRoles.FormattingEnabled = true;
-            this.comboBoxRoles.Location = new System.Drawing.Point(218, 84);
+            this.comboBoxRoles.Location = new System.Drawing.Point(216, 20);
             this.comboBoxRoles.Name = "comboBoxRoles";
             this.comboBoxRoles.Size = new System.Drawing.Size(128, 21);
             this.comboBoxRoles.TabIndex = 5;
             // 
             // textBoxSalary
             // 
-            this.textBoxSalary.Location = new System.Drawing.Point(31, 84);
+            this.textBoxSalary.Location = new System.Drawing.Point(29, 20);
             this.textBoxSalary.Name = "textBoxSalary";
             this.textBoxSalary.Size = new System.Drawing.Size(116, 20);
             this.textBoxSalary.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(218, 127);
+            this.button1.Location = new System.Drawing.Point(216, 63);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 23);
             this.button1.TabIndex = 3;
@@ -217,48 +175,59 @@
             // 
             // buttonSalary
             // 
-            this.buttonSalary.Location = new System.Drawing.Point(25, 127);
+            this.buttonSalary.Location = new System.Drawing.Point(23, 63);
             this.buttonSalary.Name = "buttonSalary";
             this.buttonSalary.Size = new System.Drawing.Size(128, 23);
             this.buttonSalary.TabIndex = 2;
             this.buttonSalary.Text = "Change Salary";
             this.buttonSalary.UseVisualStyleBackColor = true;
             // 
-            // label4
+            // comboBoxID
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(51, 27);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(18, 13);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "ID";
+            this.comboBoxID.Enabled = false;
+            this.comboBoxID.FormattingEnabled = true;
+            this.comboBoxID.Location = new System.Drawing.Point(392, 123);
+            this.comboBoxID.Name = "comboBoxID";
+            this.comboBoxID.Size = new System.Drawing.Size(172, 21);
+            this.comboBoxID.TabIndex = 3;
+            this.comboBoxID.Visible = false;
             // 
-            // comboBox2
+            // labelID
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(120, 24);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(172, 21);
-            this.comboBox2.TabIndex = 0;
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(341, 126);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(18, 13);
+            this.labelID.TabIndex = 3;
+            this.labelID.Text = "ID";
+            this.labelID.Visible = false;
+            // 
+            // panelAdd
+            // 
+            this.panelAdd.Location = new System.Drawing.Point(262, 101);
+            this.panelAdd.Name = "panelAdd";
+            this.panelAdd.Size = new System.Drawing.Size(375, 211);
+            this.panelAdd.TabIndex = 26;
             // 
             // AdminStaffMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.panelAdAdmin);
-            this.Controls.Add(this.panelStaffdel);
+            this.Controls.Add(this.panelAdd);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.comboBoxID);
             this.Controls.Add(this.buttonAdmin);
             this.Controls.Add(this.buttonProfile);
             this.Controls.Add(this.buttonRemove);
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panelManager);
+            this.Controls.Add(this.panelAdAdmin);
+            this.Controls.Add(this.panelStaffdel);
             this.Name = "AdminStaffMenu";
             this.Size = new System.Drawing.Size(719, 376);
             this.panelAdAdmin.ResumeLayout(false);
-            this.panelAdAdmin.PerformLayout();
             this.panelStaffdel.ResumeLayout(false);
-            this.panelStaffdel.PerformLayout();
             this.panelManager.ResumeLayout(false);
             this.panelManager.PerformLayout();
             this.ResumeLayout(false);
@@ -270,12 +239,8 @@
 
         private System.Windows.Forms.Panel panelAdAdmin;
         private System.Windows.Forms.Button buttonAddadmin;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.Panel panelStaffdel;
         private System.Windows.Forms.Button buttonDel;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button buttonAdmin;
         private System.Windows.Forms.Button buttonProfile;
         private System.Windows.Forms.Button buttonRemove;
@@ -286,7 +251,8 @@
         private System.Windows.Forms.TextBox textBoxSalary;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button buttonSalary;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBoxID;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Panel panelAdd;
     }
 }
--- DBapplication/AdminStaffMenu.cs
+++ DBapplication/AdminStaffMenu.cs
@@ -18,28 +18,51 @@ namespace DBapplication
             panelStaffdel.Hide();
             panelManager.Hide();
             panelAdAdmin.Hide();
+            panelAdd.Hide();
         }
 
         private void buttonRemove_Click(object sender, EventArgs e)
         {
             panelManager.Hide();
             panelAdAdmin.Hide();
+            panelAdd.Hide();
             panelStaffdel.Show();
+            labelID.Visible = true;
+            comboBoxID.Visible = true;
+            comboBoxID.Enabled = true;
         }
 
         private void buttonProfile_Click(object sender, EventArgs e)
         {
             panelStaffdel.Hide();
             panelAdAdmin.Hide();
+            panelAdd.Hide();
             panelManager.Show();
+            labelID.Visible = true;
+            comboBoxID.Visible = true;
+            comboBoxID.Enabled = true;
         }
 
         private void buttonAdmin_Click(object sender, EventArgs e)
         {
             panelStaffdel.Hide();
             panelManager.Hide();
+            panelAdd.Hide();
             panelAdAdmin.Show();
+            labelID.Visible = true;
+            comboBoxID.Visible = true;
+            comboBoxID.Enabled = true;
         }
 
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            panelStaffdel.Hide();
+            panelManager.Hide();
+            panelAdAdmin.Hide();
+            panelAdd.Show();
+            labelID.Visible = false;
+            comboBoxID.Visible = false;
+            comboBoxID.Enabled = false;
+        }
     }
 }
