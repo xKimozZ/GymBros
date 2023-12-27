fa03eb71b392b6520983cdba60a1480f89f50db9
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2023-12-28 01:37:46 +0200

    all good now

--- DBapplication/Controller.cs
+++ DBapplication/Controller.cs
@@ -766,6 +766,34 @@ namespace DBapplication
             return dbMan.ExecuteNonQuery(query);
         }
 
+        public int AlreadyUseService(int UID, string serviceType)
+        {
+            string query = $"SELECT COUNT(*) FROM Uses_Service WHERE Member_ID = {UID} AND Service_Name = '{serviceType}';";
+            return Convert.ToInt32(dbMan.ExecuteScalar(query));
+        }
+
+        public DateTime GetSubscriptionRenewal(int UID, string serviceType)
+        {
+            string query = $"SELECT Subscription_Len FROM Uses_Service WHERE Member_ID = {UID} AND Service_Name = '{serviceType}';";
+            return (DateTime)dbMan.ExecuteScalar(query);
+        }
+
+        public int InsertServiceUsing(int UID, string serviceType, DateTime newdate)
+        {
+            string insertQuery = $"INSERT INTO Uses_Service " +
+                $"(Member_ID, Service_Name, Subscription_Len, Num_Attended_Sessions)\r\n" +
+                $"VALUES ({UID}, '{serviceType}', '{newdate}', 0);";
+            return dbMan.ExecuteNonQuery(insertQuery);
+        }
+
+        public int UpdateServiceRenewal(DateTime newdate, int UID)
+        {
+
+            string userUpdateQuery = $"UPDATE Uses_Service SET Subscription_Len = '{newdate}' WHERE Member_ID = {UID};";
+
+            return dbMan.ExecuteNonQuery(userUpdateQuery);
+        }
+
         public void TerminateConnection()
         {
             dbMan.CloseConnection();
