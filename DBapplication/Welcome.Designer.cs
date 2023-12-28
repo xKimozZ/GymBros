a6ad41cdd7ce7826ec5bd6965e5bebb43f1b18dc
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2023-12-28 05:26:23 +0200

    removed admin and staff debug logins

--- DBapplication/Welcome.Designer.cs
+++ DBapplication/Welcome.Designer.cs
@@ -28,27 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F);
-            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
-            this.button1.Image = global::DBapplication.Properties.Resources._360_F_166620979_YEjNRefWLF42MUvr42oE53HynM52RHSp;
-            this.button1.Location = new System.Drawing.Point(86, 145);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(244, 96);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Admin Debug";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -56,12 +39,12 @@
             this.button2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.AliceBlue;
             this.button2.Image = global::DBapplication.Properties.Resources.abstract_geometric_blue_background_vector_3958327;
-            this.button2.Location = new System.Drawing.Point(86, 273);
+            this.button2.Location = new System.Drawing.Point(92, 181);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(244, 87);
+            this.button2.Size = new System.Drawing.Size(332, 147);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Member Login";
+            this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -78,31 +61,16 @@
             this.label1.Text = "Welcome to <GymBros>";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.Font = new System.Drawing.Font("Segoe UI", 16.2F);
-            this.button3.ForeColor = System.Drawing.Color.AliceBlue;
-            this.button3.Image = global::DBapplication.Properties.Resources._360_F_166620979_YEjNRefWLF42MUvr42oE53HynM52RHSp;
-            this.button3.Location = new System.Drawing.Point(565, 145);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(244, 96);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Staff Debug";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.Transparent;
             this.button4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.AliceBlue;
-            this.button4.Image = global::DBapplication.Properties.Resources.abstract_geometric_blue_background_vector_3958327;
-            this.button4.Location = new System.Drawing.Point(565, 273);
+            this.button4.Image = global::DBapplication.Properties.Resources._360_F_166620979_YEjNRefWLF42MUvr42oE53HynM52RHSp;
+            this.button4.Location = new System.Drawing.Point(474, 181);
             this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(244, 87);
+            this.button4.Size = new System.Drawing.Size(341, 147);
             this.button4.TabIndex = 4;
             this.button4.Text = "Signup";
             this.button4.UseVisualStyleBackColor = false;
@@ -115,10 +83,8 @@
             this.BackgroundImage = global::DBapplication.Properties.Resources.istockphoto_610855984_612x612;
             this.ClientSize = new System.Drawing.Size(903, 438);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Welcome";
             this.Text = "<GymBros> Frontend";
@@ -128,11 +94,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
     }
 }
